80bc835cd4ca3786cd7be842a8b1c711714573d7
AdityaBare <bareaditya0@gmail.com>
2026-07-17 16:49:58 +0530

    First Assignment

--- Assignment1.py
+++ Assignment1.py
@@ -26,3 +26,21 @@ print(arr.strides)
 ones = np.ones((3,4))
 print(ones)
 
+arr = np.array([1,2,3,4,5])
+print(f"Arrays={arr}\n")
+print(f"SUM={np.sum(arr)}\n")
+print(f"MIN={np.min(arr)}\n")
+print(f"MAX={np.max(arr)}\n")
+print(f"PRODUCT={np.prod(arr)}\n")
+print(f"STANDERD DEVIATION={np.std(arr)}\n")
+print(f"VARIANCE={np.var(arr)}\n")
+print(f"MEAN={np.mean(arr)}\n")
+print(f"ZEROS={np.zeros((2,3))}\n")
+print(f"FULL={np.full(2,5),10}\n")
+print(f"ONCE={np.ones((2,3))}\n")
+print(f"ARANGE={np.arange(1,10,2)}\n")
+print(f"3D Array={arr3}\n")
+print(f"LINESPACE={np.linspace(1,10,5)}\n")
+
+
+
